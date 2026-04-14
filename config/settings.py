62a14529f9695a6a8f54d7d6c1d2d464319884f2
Kirill <kmalyautskiy@bk.ru>
2026-04-14 20:40:15 +0500

    allowed host

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOST', '127.0.0.1')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
