c57a2bcf99fd651ef1b425fa859666d5452e4462
Kirill <kmalyautskiy@bk.ru>
2026-04-14 23:35:40 +0500

    media

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('page/<slug:slug>/', views.page_detail, name='page_detail'),
     path('api/update-reception-id/', views.update_reception_id, name='update_reception_id'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
